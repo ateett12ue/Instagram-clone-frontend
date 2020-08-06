0681ff658605b6754e87c0b254637b1a126dc58f
ateet <ateet@propelld.com>
2020-08-06 22:01:59 +0530

    CORS Policy issue, added proxy, add a letter next

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,20 +1,58 @@
-import React from "react";
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const Postdata = () => {
+    const url = "/signup"
+    fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "",
+        password: "",
+        email: "",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div className="mycard">
       <div className="card auth-card input-field">
         <h2>Instagram</h2>
-        <input type="text" placeholder="name"/>
-        <input type="text" placeholder="email"/>
-        <input type="text" placeholder="password"/>
-        <button className="btn waves-effect #64b5f6 blue
- waves-light darken-1">
-            Signup
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="btn waves-effect #64b5f6 blue waves-light darken-1"
+          onClick={()=>Postdata()}
+        >
+          Signup
         </button>
         <h5>
-            <Link to="/signin">Already have an account ?</Link>
+          <Link to="/signin">Already have an account ?</Link>
         </h5>
       </div>
     </div>
