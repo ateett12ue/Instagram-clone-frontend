a711025013efab8d370f5288f11136f7a9106779
ateet <ateet@propelld.com>
2020-08-07 00:56:39 +0530

    basic jwt validation added, need refractring

--- src/screens/CreatePost.js
+++ src/screens/CreatePost.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {useHistory} from 'react-router-dom'
 import M from "materialize-css"
 
@@ -8,7 +8,35 @@ export default function CreatePost() {
   const [body, setBody] = useState("");
   const [image, setImage] = useState("");
   const [url, setUrl] = useState("")
-  
+  useEffect(()=>{
+    if(url){
+    fetch('/createpost', {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+      },
+      body: JSON.stringify({
+        title,
+        body,
+        pic:url
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if(data.error){
+          M.toast({html: data.error, classes:"#c62828 red darken-3"})
+        }
+        else{
+          M.toast({html:"Post Upload Successfully", classes:"#43a047 green darken-1"})
+          history.push('/')
+        }
+      }).catch(err=>{
+        console.log(err)
+      })
+    }
+  },[url])
+
   const postDetail = () => {
     const data = new FormData();
     data.append("file", image);
@@ -25,30 +53,6 @@ export default function CreatePost() {
     .catch(err=>{
       console.log(err)
     });
-
-    fetch('/createpost', {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title,
-        body,
-        url
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if(data.error){
-          M.toast({html: data.error, classes:"#c62828 red darken-3"})
-        }
-        else{
-          M.toast({html:"Post Upload Successfully", classes:"#43a047 green darken-1"})
-          history.push('/')
-        }
-      }).catch(err=>{
-        console.log(err)
-      })
   };
 
   return (
