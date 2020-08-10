dc642afd930aaab23b3b286820dbe98f67459fd7
ateet <ateet@propelld.com>
2020-08-11 02:45:00 +0530

    added like unlike logic

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../App";
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const { state, dispatch } = useContext(UserContext);
   useEffect(() => {
+    console.log("user", state)
     fetch("/allpost", {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("jwt"),
@@ -13,6 +16,63 @@ export default function Home() {
         setData(result.post);
       });
   }, []);
+
+  const likePost = (id) => {
+    console.log(state)
+    fetch("/like", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const unlikePost = (id) => {
+    fetch("/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="home">
       {data.map((item) => {
@@ -23,9 +83,29 @@ export default function Home() {
               <img src={item.photo} />
             </div>
             <div className="card-content">
-              <i className="material-icons" style={{ color: "red" }}>
-                favorite
-              </i>
+              {item.likes.includes(state.id) ? (
+                <i
+                  className="material-icons"
+                  style={{ color: "red", cursor: "pointer" }}
+                  onClick={() => {
+                    unlikePost(item._id);
+                  }}
+                >
+                  favorite
+                </i>
+              ) : (
+                <i
+                  className="material-icons"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    likePost(item._id);
+                  }}
+                >
+                  favorite_border
+                </i>
+              )}
+
+              <h6>{item.likes.length} likes</h6>
               <h6>{item.title}</h6>
               <p>{item.body}</p>
               <input type="text" placeholder="add a comment" />
