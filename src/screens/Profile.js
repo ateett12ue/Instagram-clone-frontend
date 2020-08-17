fdede10458429c55ef20f8ab88d304ffefe385a3
ateet <ateet@propelld.com>
2020-08-17 18:15:12 +0530

    added change profile pic

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,8 +3,9 @@ import { UserContext } from "../App";
 export default function Profile() {
   const [mypics, setpics] = useState([]);
   const { state, dispatch } = useContext(UserContext);
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState(undefined);
   useEffect(() => {
-    
     fetch("/mypost", {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("jwt"),
@@ -16,13 +17,39 @@ export default function Profile() {
       });
   }, []);
   const titleCase = (str) => {
-    var splitStr = str.toLowerCase().split(' ');
+    var splitStr = str.toLowerCase().split(" ");
     for (var i = 0; i < splitStr.length; i++) {
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+      splitStr[i] =
+        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
     }
-    return splitStr.join(' '); 
- }
- console.log(state)
+    return splitStr.join(" ");
+  };
+  useEffect(() => {
+    if (image) {
+      const data = new FormData();
+      data.append("file", image);
+      data.append("upload_preset", "instagram-clone-app");
+      data.append("cloud_name", "ateet");
+      fetch("https://api.cloudinary.com/v1_1/ateet/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUrl(data.url);
+          console.log(data)
+          localStorage.setItem("user", JSON.stringify({...state, pic: data.url}))
+          dispatch({type:"UPDATEPIC", payload: data.url})
+          window.location.reload()
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [image]);
+  const updateProfilePic = (file) => {
+    setImage(file);
+  };
   return (
     <div style={{ maxWidth: "550px", margin: "0px auto" }}>
       <div
@@ -36,11 +63,12 @@ export default function Profile() {
         <div>
           <img
             style={{ width: "160px", height: "160px", borderRadius: "80px" }}
-            src={state?state.pic:"no imgae"}
+            src={state ? state.pic : "no imgae"}
           />
         </div>
+
         <div>
-          <h4>{titleCase(state?state.name:"loading")}</h4>
+          <h4>{titleCase(state ? state.name : "loading")}</h4>
           <div
             style={{
               display: "flex",
@@ -49,11 +77,27 @@ export default function Profile() {
             }}
           >
             <h6>{mypics.length} Post</h6>
-            <h6>{state? state.followers.length : "NA"} Followers</h6>
-            <h6>{state? state.following.length: "NA"} Following</h6>
+            <h6>{state ? state.followers.length : "NA"} Followers</h6>
+            <h6>{state ? state.following.length : "NA"} Following</h6>
+          </div>
+        </div>
+        <div className="file-field input-field">
+          <div className="btn #64b5f6 blue darken-1">
+            <span>Upload Profile Image</span>
+            <input
+              type="file"
+              placeholder="Input Image"
+              onChange={(e) => {
+                updateProfilePic(e.target.files[0]);
+              }}
+            />
+          </div>
+          <div className="file-path-wrapper">
+            <input className="file-path validate" type="text" />
           </div>
         </div>
       </div>
+
       <div className="gallery">
         {mypics.map((item) => {
           return (
